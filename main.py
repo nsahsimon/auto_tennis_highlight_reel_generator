598e73f5db681254e5e37c416fa93a2e89a6ac72
nsahsimon <nsahsimon01@gmail.com>
2023-03-07 18:18:48 +0100

    first commit

--- main.py
+++ main.py
@@ -7,12 +7,161 @@ import numpy as np
 import matplotlib.pyplot as plt
 import sys
 sys.path.insert(1, 'src')
-import models.frame as fr
+# import models.frame as fr
 import moviepy.editor as mp
 from moviepy.video.io.VideoFileClip import VideoFileClip
 from moviepy.video.compositing.concatenate import concatenate_videoclips
-import ocr
+# import ocr
 import paddleocr
+from PIL import Image, ImageDraw, ImageFont
+
+
+
+# Frame class
+
+
+class Frame:
+    count = None # the position of the frame in the list of all frames in the video
+    timestamp = None # the timestamp of the frame given the current frame rate
+    fps = None # the frame rate of the video
+    data = [None, None, None] # a tuple containing the game score contained in the frame (upDigits, downDigits, edgeXCoord)
+    image = None
+    
+    def __init__(self, count : np.random.randint(1, 100) = 10, timestamp: int = np.random.randint(1, 100), fps : float= 0.1 * np.random.randint(1, 100), data: list = [], image: np.ndarray = np.zeros((3,3),dtype=np.uint8)):
+        self.count = count
+        self.timestamp = timestamp
+        self.fps = fps
+        self.data = data
+        self.image = image
+
+    def classicTimestamp(self):
+        return time.strftime("%H:%M:%S", time.gmtime(self.timestamp))
+    
+    def setEdge_x(self, edge_x):
+        self.data[2] = edge_x
+
+
+
+# OCR LOGIC
+
+def extractScoreBoard(image):
+    # relative coordinates of the top left corner of the scoreboard
+    topLeftRel = (0.017, 0.842) 
+
+    # relative coordinates of the bottom right corner of the scoreboard
+    bottomRightRel = (0.364, 0.95)
+
+    # extracting image dimensions
+    width = image.shape[1]
+    height = image.shape[0]
+
+    # calculating the absolute scoreboard corner coordinates
+    topLeft = (int(width * topLeftRel[0]), int(height * topLeftRel[1]))
+    bottomRight = (int(width * bottomRightRel[0]), int(height * bottomRightRel[1]))
+
+
+    # extract the score board 
+    scoreBoard = image[topLeft[1] : bottomRight[1], topLeft[0] : bottomRight[0],:]
+
+    return scoreBoard
+
+
+def decodeResults(results):
+    predictions = results[0]
+    boxes = [prediction[0] for prediction in predictions ]
+    txts = [prediction[1][0] for prediction in predictions]
+    scores = [prediction[1][1] for prediction in predictions]
+    return {'boxes': boxes,'txts': txts, 'scores': scores}
+
+
+def draw_boxes(image, boxes, txts, scores, drop_score=0.5, font_path=None):
+    """
+    Draw bounding boxes and text on the input image.
+    """
+    font = ImageFont.truetype(font_path, size=16) if font_path else ImageFont.load_default()
+    for i, box in enumerate(boxes):
+        # Convert score to float if it's a string
+        score = float(scores[i]) if isinstance(scores[i], str) else scores[i]
+        if scores is not None and (score < drop_score or math.isnan(score)):
+            continue
+        tl = box[0]
+        br = box[2]
+        # Draw bounding box
+        cv2.rectangle(image, (int(tl[0]), int(tl[1])), (int(br[0]), int(br[1])), (0,0, 255), 2)
+        # Draw text
+        # text = txts[i][0]
+        # cv2.putText(image, text, (xy[0], xy[1]), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+    return image
+
+
+def findScoreBoardEdge(image):
+
+    blur_image = cv2.GaussianBlur(image, (3,3), 0.01)
+
+    canny_image = cv2.Canny(blur_image, 115, 150)    
+    
+    height, width = canny_image.shape
+
+    # assert(channels == 1)
+
+    max_dist = 0
+
+    # Loop over each row and each column of the image
+    for y in range(height):
+        for x in range(width):
+            # Get the color of the pixel at (x, y)
+            # check if the pixel is white
+            if canny_image[y][x] == 255: 
+                if x > max_dist:
+                    max_dist = x
+
+    edgeXCoordRel = max_dist / image.shape[1]
+
+    # print(f"Max distance: {max_dist}")
+    return edgeXCoordRel
+
+
+def extractFrameData(src, ocr):
+    image = extractScoreBoard(src)
+    original = image
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    width = image.shape[1] ; height = image.shape[0]
+    width_of_dark = int (0.09 * width)
+    edge_x = int (findScoreBoardEdge(image) * width)
+    start_y = 0; end_y = height
+    start_x = edge_x - width_of_dark ; end_x = edge_x
+    image[start_y : end_y,start_x : end_x] = cv2.bitwise_not(image[start_y : end_y,start_x : end_x])
+    image = cv2.bitwise_not(image)
+    image = cv2.equalizeHist(image, )
+    new_start_x = int(edge_x - 0.14 * width)
+    image = original[start_y : end_y,  new_start_x : edge_x]
+    image = cv2.resize(image, (100, 100))
+    results = ocr.ocr(image)
+    results = decodeResults(results)
+    txts = results['txts']
+    print(txts)
+    up = None; down = None
+    if len(txts) == 4:
+        up = [txts[0] , txts[1]]
+        down = [txts[2], txts[3]]
+    return [up, down, edge_x]
+
+
+# test function
+def test(self):
+    file_names = os.listdir(path='images/')
+    for i, name in enumerate(file_names):
+        full_path = os.path.join('images/', name)
+        image = cv2.imread(full_path)
+        data = self.extractFrameData(image)
+        print(f"{i + 1}: {name}")
+        print(f" up: {data[0]} \n down: {data[1]} \n edge_x: {data[2]} \n")
+        cv2.imshow("image", image)
+        cv2.waitKey()
+        cv2.destroyAllWindows()
+
+
+
 
 
 # Load paddle ocr
@@ -71,14 +220,7 @@ def getSampleFrames(path, numberOfSamples=5, samplePeriod=5): # sample period de
         if sampleCount > numberOfSamples:
             break
         frameCount += 1
-
-
-    if isSingleChannel == True:
-        images = [cv2.merge((image, image, image)) for image in images]
-
-    predictions = pipeline.recognize(images)
-
-    return ["".join([prediction[0] for prediction in predictions[i]]) for i in range(len(predictions)) ]
+    return
 
 def processFrames(path: str, sampleTime: int = DEFAULT_SAMPLE_TIME, samplePeriod:int=15):
         sampledFrames = []
@@ -119,7 +261,7 @@ def processFrames(path: str, sampleTime: int = DEFAULT_SAMPLE_TIME, samplePeriod
                 # cv2.imshow(f"Sample: {sampleCount} of {numberOfSamples}",frame)
                 # cv2.waitKey(10)
 
-                newFrameData = ocr.extractFrameData(frame, ocr=OCR)
+                newFrameData = extractFrameData(frame, ocr=OCR)
                 
 
                 # ignore corrupt data
@@ -132,7 +274,7 @@ def processFrames(path: str, sampleTime: int = DEFAULT_SAMPLE_TIME, samplePeriod
 
                 print(f"edge_x: {newFrameData[2]}")
 
-                newFrame = fr.Frame(
+                newFrame = Frame(
                     count=frameCount,
                     timestamp= calcTimestamp(fps, frameCount),
                     fps=fps,
@@ -157,7 +299,7 @@ def processFrames(path: str, sampleTime: int = DEFAULT_SAMPLE_TIME, samplePeriod
         
         return sampledFrames
 
-def hasChanged_set(prevFrame: fr.Frame, currentFrame: fr.Frame):
+def hasChanged_set(prevFrame: Frame, currentFrame: Frame):
     prevEdgeXCoord = prevFrame.data[2]
     currentEdgeXCoord = currentFrame.data[2]
     if abs(prevEdgeXCoord - currentEdgeXCoord) >= SET_CHANGE_THRESH:
@@ -166,7 +308,7 @@ def hasChanged_set(prevFrame: fr.Frame, currentFrame: fr.Frame):
     else:
         return False
 
-def hasChanged_game(prevFrame: fr.Frame, currentFrame: fr.Frame):
+def hasChanged_game(prevFrame: Frame, currentFrame: Frame):
     prevTopleftDigits = prevFrame.data[0][0]
     prevBottomleftDigits = prevFrame.data[1][0]
     currentTopleftDigits = currentFrame.data[0][0]
@@ -176,7 +318,7 @@ def hasChanged_game(prevFrame: fr.Frame, currentFrame: fr.Frame):
     else:
         return False
 
-def hasChanged_point(prevFrame: fr.Frame, currentFrame: fr.Frame):
+def hasChanged_point(prevFrame: Frame, currentFrame: Frame):
     prevToprightDigits = prevFrame.data[0][1]
     prevBottomrightDigits = prevFrame.data[1][1]
     currentToprightDigits = currentFrame.data[0][1]
@@ -403,7 +545,7 @@ def generateSubclipIntervals(data: list, subclipIndices: list, pointOffset: int 
     return sortedClipIntervals
 
 
-def findEffectiveClipInterval(startFrame: fr.Frame, stopFrame: fr.Frame, pointOffset: int = 15 , oddGameOffset: int = 70, samplePeriod: int = 5):
+def findEffectiveClipInterval(startFrame: Frame, stopFrame: Frame, pointOffset: int = 15 , oddGameOffset: int = 70, samplePeriod: int = 5):
     start_time = startFrame.timestamp 
     stop_time = stopFrame.timestamp
     is_odd_game = False
@@ -474,3 +616,14 @@ def run_app():
 
 
 run_app()
+
+
+
+
+
+
+
+
+
+
+
--- src/ocr.py
+++ src/ocr.py
@@ -1,132 +1,132 @@
-import cv2
-import numpy as np
-import paddleocr
-import math
-import os
-from PIL import Image, ImageDraw, ImageFont
+# import cv2
+# import numpy as np
+# import paddleocr
+# import math
+# import os
+# from PIL import Image, ImageDraw, ImageFont
 
-# print("Initializing OCR Engine")
-# ocr = paddleocr.PaddleOCR(lang='en')
-# print("Paddle OCR object successfully created")
+# # print("Initializing OCR Engine")
+# # ocr = paddleocr.PaddleOCR(lang='en')
+# # print("Paddle OCR object successfully created")
 
-def extractScoreBoard(image):
-    # relative coordinates of the top left corner of the scoreboard
-    topLeftRel = (0.017, 0.842) 
+# def extractScoreBoard(image):
+#     # relative coordinates of the top left corner of the scoreboard
+#     topLeftRel = (0.017, 0.842) 
 
-    # relative coordinates of the bottom right corner of the scoreboard
-    bottomRightRel = (0.364, 0.95)
+#     # relative coordinates of the bottom right corner of the scoreboard
+#     bottomRightRel = (0.364, 0.95)
 
-    # extracting image dimensions
-    width = image.shape[1]
-    height = image.shape[0]
+#     # extracting image dimensions
+#     width = image.shape[1]
+#     height = image.shape[0]
 
-    # calculating the absolute scoreboard corner coordinates
-    topLeft = (int(width * topLeftRel[0]), int(height * topLeftRel[1]))
-    bottomRight = (int(width * bottomRightRel[0]), int(height * bottomRightRel[1]))
-
-
-    # extract the score board 
-    scoreBoard = image[topLeft[1] : bottomRight[1], topLeft[0] : bottomRight[0],:]
-
-    return scoreBoard
+#     # calculating the absolute scoreboard corner coordinates
+#     topLeft = (int(width * topLeftRel[0]), int(height * topLeftRel[1]))
+#     bottomRight = (int(width * bottomRightRel[0]), int(height * bottomRightRel[1]))
 
 
-def decodeResults(results):
-    predictions = results[0]
-    boxes = [prediction[0] for prediction in predictions ]
-    txts = [prediction[1][0] for prediction in predictions]
-    scores = [prediction[1][1] for prediction in predictions]
-    return {'boxes': boxes,'txts': txts, 'scores': scores}
+#     # extract the score board 
+#     scoreBoard = image[topLeft[1] : bottomRight[1], topLeft[0] : bottomRight[0],:]
+
+#     return scoreBoard
 
 
-def draw_boxes(image, boxes, txts, scores, drop_score=0.5, font_path=None):
-    """
-    Draw bounding boxes and text on the input image.
-    """
-    font = ImageFont.truetype(font_path, size=16) if font_path else ImageFont.load_default()
-    for i, box in enumerate(boxes):
-        # Convert score to float if it's a string
-        score = float(scores[i]) if isinstance(scores[i], str) else scores[i]
-        if scores is not None and (score < drop_score or math.isnan(score)):
-            continue
-        tl = box[0]
-        br = box[2]
-        # Draw bounding box
-        cv2.rectangle(image, (int(tl[0]), int(tl[1])), (int(br[0]), int(br[1])), (0,0, 255), 2)
-        # Draw text
-        # text = txts[i][0]
-        # cv2.putText(image, text, (xy[0], xy[1]), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
-    return image
+# def decodeResults(results):
+#     predictions = results[0]
+#     boxes = [prediction[0] for prediction in predictions ]
+#     txts = [prediction[1][0] for prediction in predictions]
+#     scores = [prediction[1][1] for prediction in predictions]
+#     return {'boxes': boxes,'txts': txts, 'scores': scores}
 
 
-def findScoreBoardEdge(image):
+# def draw_boxes(image, boxes, txts, scores, drop_score=0.5, font_path=None):
+#     """
+#     Draw bounding boxes and text on the input image.
+#     """
+#     font = ImageFont.truetype(font_path, size=16) if font_path else ImageFont.load_default()
+#     for i, box in enumerate(boxes):
+#         # Convert score to float if it's a string
+#         score = float(scores[i]) if isinstance(scores[i], str) else scores[i]
+#         if scores is not None and (score < drop_score or math.isnan(score)):
+#             continue
+#         tl = box[0]
+#         br = box[2]
+#         # Draw bounding box
+#         cv2.rectangle(image, (int(tl[0]), int(tl[1])), (int(br[0]), int(br[1])), (0,0, 255), 2)
+#         # Draw text
+#         # text = txts[i][0]
+#         # cv2.putText(image, text, (xy[0], xy[1]), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+#     return image
 
-    blur_image = cv2.GaussianBlur(image, (3,3), 0.01)
 
-    canny_image = cv2.Canny(blur_image, 115, 150)    
+# def findScoreBoardEdge(image):
+
+#     blur_image = cv2.GaussianBlur(image, (3,3), 0.01)
+
+#     canny_image = cv2.Canny(blur_image, 115, 150)    
     
-    height, width = canny_image.shape
+#     height, width = canny_image.shape
 
-    # assert(channels == 1)
+#     # assert(channels == 1)
 
-    max_dist = 0
+#     max_dist = 0
 
-    # Loop over each row and each column of the image
-    for y in range(height):
-        for x in range(width):
-            # Get the color of the pixel at (x, y)
-            # check if the pixel is white
-            if canny_image[y][x] == 255: 
-                if x > max_dist:
-                    max_dist = x
+#     # Loop over each row and each column of the image
+#     for y in range(height):
+#         for x in range(width):
+#             # Get the color of the pixel at (x, y)
+#             # check if the pixel is white
+#             if canny_image[y][x] == 255: 
+#                 if x > max_dist:
+#                     max_dist = x
 
-    edgeXCoordRel = max_dist / image.shape[1]
+#     edgeXCoordRel = max_dist / image.shape[1]
 
-    # print(f"Max distance: {max_dist}")
-    return edgeXCoordRel
+#     # print(f"Max distance: {max_dist}")
+#     return edgeXCoordRel
   
 
-def extractFrameData(src, ocr):
-    image = extractScoreBoard(src)
-    original = image
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    width = image.shape[1] ; height = image.shape[0]
-    width_of_dark = int (0.09 * width)
-    edge_x = int (findScoreBoardEdge(image) * width)
-    start_y = 0; end_y = height
-    start_x = edge_x - width_of_dark ; end_x = edge_x
-    image[start_y : end_y,start_x : end_x] = cv2.bitwise_not(image[start_y : end_y,start_x : end_x])
-    image = cv2.bitwise_not(image)
-    image = cv2.equalizeHist(image, )
-    new_start_x = int(edge_x - 0.14 * width)
-    image = original[start_y : end_y,  new_start_x : edge_x]
-    image = cv2.resize(image, (100, 100))
-    results = ocr.ocr(image)
-    results = decodeResults(results)
-    txts = results['txts']
-    print(txts)
-    up = None; down = None
-    if len(txts) == 4:
-        up = [txts[0] , txts[1]]
-        down = [txts[2], txts[3]]
-    return [up, down, edge_x]
+# def extractFrameData(src, ocr):
+#     image = extractScoreBoard(src)
+#     original = image
+#     image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+#     width = image.shape[1] ; height = image.shape[0]
+#     width_of_dark = int (0.09 * width)
+#     edge_x = int (findScoreBoardEdge(image) * width)
+#     start_y = 0; end_y = height
+#     start_x = edge_x - width_of_dark ; end_x = edge_x
+#     image[start_y : end_y,start_x : end_x] = cv2.bitwise_not(image[start_y : end_y,start_x : end_x])
+#     image = cv2.bitwise_not(image)
+#     image = cv2.equalizeHist(image, )
+#     new_start_x = int(edge_x - 0.14 * width)
+#     image = original[start_y : end_y,  new_start_x : edge_x]
+#     image = cv2.resize(image, (100, 100))
+#     results = ocr.ocr(image)
+#     results = decodeResults(results)
+#     txts = results['txts']
+#     print(txts)
+#     up = None; down = None
+#     if len(txts) == 4:
+#         up = [txts[0] , txts[1]]
+#         down = [txts[2], txts[3]]
+#     return [up, down, edge_x]
 
 
-# test function
-def test():
-    file_names = os.listdir(path='images/')
-    for i, name in enumerate(file_names):
-        full_path = os.path.join('images/', name)
-        image = cv2.imread(full_path)
-        data = extractFrameData(image)
-        print(f"{i + 1}: {name}")
-        print(f" up: {data[0]} \n down: {data[1]} \n edge_x: {data[2]} \n")
-        cv2.imshow("image", image)
-        cv2.waitKey()
-        cv2.destroyAllWindows()
+# # test function
+# def test():
+#     file_names = os.listdir(path='images/')
+#     for i, name in enumerate(file_names):
+#         full_path = os.path.join('images/', name)
+#         image = cv2.imread(full_path)
+#         data = extractFrameData(image)
+#         print(f"{i + 1}: {name}")
+#         print(f" up: {data[0]} \n down: {data[1]} \n edge_x: {data[2]} \n")
+#         cv2.imshow("image", image)
+#         cv2.waitKey()
+#         cv2.destroyAllWindows()
 
 
-# test()
+# # test()
 
 
 
